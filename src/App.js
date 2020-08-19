a441f3e51c9509addd9fd7cff6d2617a7bedc48c
Giovanna Caivano <giovannadottocaivano@gmail.com>
2020-08-19 14:05:26 -0300

    Alterações no componente pai (App.js) e FilterBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import Filter from './assets/components/FilterBar/FilterBar'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+const products = [
+  { img: "src", name: "Item 1", price: 100},
+  { img: "src", name: "Item 2", price: 90},
+  { img: "src", name: "Item 3", price: 80},
+  { img: "src", name: "Item 4", price: 50},
+  { img: "src", name: "Item 5", price: 110},
+  { img: "src", name: "Item 6", price: 30},
+  { img: "src", name: "Item 7", price: 100},
+  { img: "src", name: "Item 8", price: 70}
+]
+
+class App extends React.Component {
+
+  state = {
+    products: products,
+    valorInputMin: "",
+    valorInputMax: ""
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Filter/>
+        <div>
+          {this.state.products.map((prod) => {
+            return <div>{prod.name}</div>
+          })}
+          </div>
+        {/* GRID DE PRODUTOS */}
+        {/* SHOPPING CART RENDERIZADO CONDICIONALMENTE       */}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/assets/components/FilterBar/FilterBar.js
+++ src/assets/components/FilterBar/FilterBar.js
@@ -1,2 +1,59 @@
 import React from 'react'
 import styled from 'styled-components'
+
+const FilterDiv = styled.div`
+    width: 300px;
+    display: flex;
+    margin: 20px;
+    flex-direction: column;
+    border: solid 1px black;
+    padding: 10px;
+`
+
+const FilterBlock = styled.div`
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+`
+
+class Filter extends React.Component {
+
+    onChangeMin = (e) => {
+        //devolve valor do input para componente pai (App)
+        const valorInputMin = e.target.value
+        console.log(valorInputMin)
+    }
+    
+    onChangeMax = (e) => {
+        //devolve valor do input para componente pai (App)
+        const valorInputMax = e.target.value
+        console.log(valorInputMax)
+    }
+    render() {
+        return(
+            <FilterDiv>
+                <h2>Filtros:</h2>
+                <FilterBlock>
+                    <label>Valor Mínimo</label>
+                    <input 
+                        type="number"
+                        onChange={this.onChangeMin}
+                        />
+                </FilterBlock>
+                <FilterBlock>
+                    <label>Valor Máximo</label>
+                    <input 
+                        type="number"
+                        onChange={this.onChangeMax}
+                        />
+                </FilterBlock>
+                <FilterBlock>
+                    <label>Buscar produto</label>
+                    <input type="text"/>
+                </FilterBlock>
+            </FilterDiv>
+        )
+    }
+}
+
+export default Filter;
