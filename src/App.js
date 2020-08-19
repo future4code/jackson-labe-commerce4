df3bd34c6c0cff4b346699799be9bddde94ea150
Lucas  Duarte <lucasduarte@MacBook-Pro-de-Lucas.local>
2020-08-19 14:02:21 -0300

    Componente do card do produto criado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import ProductCard from './assets/components/ProductCard/ProductCard'
 
 function App() {
   return (
     <div className="App">
-      
+      <ProductCard />
     </div>
   );
 }
