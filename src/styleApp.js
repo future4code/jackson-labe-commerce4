630fe3f3f35eae34e2f81f48c4435d4ef94e998a
Lucas  Duarte <lucasduarte@MacBook-Pro-de-Lucas.local>
2020-08-20 12:50:34 -0300

    Filtros funcionando 100%

---
+++ src/styleApp.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const ContainerApp = styled.div`
+    display: flex;
+`
+
+export const ProductGrid = styled.span`
+  width: 50%;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  row-gap: 5px;
+  margin: 20px;
+`
+
