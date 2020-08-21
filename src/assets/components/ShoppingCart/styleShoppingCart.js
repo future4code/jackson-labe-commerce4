de723ab949fd2a896165e2647c31bf1d7a74c762
Lucas  Duarte <lucasduarte@MacBook-Pro-de-Lucas.local>
2020-08-21 13:39:53 -0300

    Carrinho já funcionagit add .

---
+++ src/assets/components/ShoppingCart/styleShoppingCart.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const CartSpan = styled.span`
+    position:relative;
+    top: 50px;
+    left: 50vw;
+    height: 50vh;
+    width:40vw;
+    text-align:center;
+
+    display: flex;
+    flex-direction: column;
+    background-color: #F0F0F0;
+    border: solid 1px #F0F0F0;
+    padding: 10px;
+`
+export const ItemsList = styled.ul`
+    width: 100%;
+`    
